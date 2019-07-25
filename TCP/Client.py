ac9076c4cafa97629244853275a210937fe998c6
Theieyrre <crizfmtfm@gmail.com>
2019-07-25 09:17:04 +0300

    Sending with packages

--- TCP/Client.py
+++ TCP/Client.py
@@ -1,11 +1,16 @@
 #!/user/bin/env python3
 
 import socket
+# For package size
 import os
+# For package count
+import math
 
 host = 'localhost'
 portSender = 3333
 portReceiver = 4444
+# Send 2KB each time
+package_size = 2048
 
 # Connect to server
 sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,14 +18,27 @@ sender.connect((host, portSender))
 print("Sender connected to port ", portSender)
 
 # Read file
-filename = "Queue.java"
+filename = "Chapter_6_V6.0.ppt"
 file = open(filename, 'rb')
-file_data = file.read()
 print("File named ", filename, " has opened")
 
 # Send file
-sentData = sender.send(file_data)
-if sentData == os.stat(filename).st_size:
+
+package_count = math.ceil(os.stat(filename).st_size / package_size)
+print("Package count ",package_count)
+
+count_sent = 0
+total_sent = 0
+
+while count_sent < package_count:
+    file_data = file.read(package_size)
+    sent_data = sender.send(file_data)
+    # sent_data is the size of the data sent
+    total_sent += sent_data
+    print("total_sent ",total_sent)
+    count_sent += 1
+    
+if total_sent == os.stat(filename).st_size:
     print("Successfully sent all data")
 else:
     print(os.stat(filename).st_size - sentData, " bytes could not sent")
@@ -31,11 +49,17 @@ print("Receiver connected to port ", portReceiver)
 
 # Receive and write file
 # Different version to check new file
-filename = "Queue2.java"
+filename = "Chapter_6_V6.1.ppt"
 file = open(filename, 'wb+')
 
 # Receive file
-receivedData = receiver.recv(2048)
+
+count_received = 0
+total_received = 0
+while count_received < count_sent:
+    received_data = receiver.recv(2048)
+    total_received += received_data
+    
 file = file.write(receivedData)
 print("Successfully created received data")
 
--- TCP/Server.py
+++ TCP/Server.py
@@ -6,6 +6,8 @@ import os
 host = "localhost"
 portA = 3333
 portB = 4444
+# Total size of the file in bytes
+total_size = 12177920
 
 socket_for_a = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Try to bind socket_for_a
@@ -17,28 +19,38 @@ except socket.error as message:
 # Set maximum client for this socket
 socket_for_a.listen(1)
 
-# Wait for data
-connectionA, addressA = socket_for_a.accept()
-print("Connected with ", portA)
-
-# Receive data from A
-data = connectionA.recv(2048)
-
 # Bind to socket_for_b
 socket_for_b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     socket_for_b.bind((host, portB))
 except socket.error as message:
-    print("Bind faile with error ", message[1])
+    print("Bind faile with error ", message[1]) 
 
 # Set maximum client for this socket wait for data
 socket_for_b.listen(1)
 connectionB, addressB = socket_for_b.accept()
 print("Connected with ", portB)
+connectionA, addressA = socket_for_a.accept()
+print("Connected with ", portA)
 
-# Send data to B
-data_sent = connectionB.send(data)
-if len(data) == data_sent:
+# Receive data from A and send data to B
+total_sent = 0
+total_received = 0
+while True:
+    data = connectionA.recv(2048)
+    total_sent += data
+    data_sent = connectionB.send(data)
+    # Basic control to change later
+    if len(data) == data_sent:
+        print("Data sent successfully")
+        total_received += data_sent
+    else:
+        print("Couldn't send data")
+    if total_sent == total_size:     
+        print("All data sent")
+        break
+    
+if total_sent == total_received:
     print("All data sent successfully")
 else:
-    print(os.stat(data).st_size - os.stat(data_sent).st_size, " bytes are failed to sent")
+    print(total_sent - total_received , " bytes are failed to sent")
